de4177fdc290252d47ccad23f3003e522336debc
Keryann LEROY <keryann.leroy@etudiant.univ-reims.fr>
2020-09-29 23:00:30 +0200

    Classes de Tile partiellement complétées.

--- Puzzle_L3/src/puzzle_l3/CasePleine.java
+++ Puzzle_L3/src/puzzle_l3/CasePleine.java
@@ -5,10 +5,41 @@
  */
 package puzzle_l3;
 
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+
 /**
  *
  * @author kerya
  */
 public class CasePleine extends Tile{
     
+    
+    private PosDouble posGraphic;
+
+    private Image image;
+    private Color highlight;
+    private boolean isHighlighted
+
+ 
+    public boolean move()
+    {
+        
+    }
+    
+    public void setHighlightColor(Color col)
+    {
+        highlight=col;
+    }
+    
+    public void toggleHighlight(boolean toggled)
+    {
+        isHighlighted=toggled;
+    }
+    
+    public void draw(GraphicsContext context)
+    {
+        
+    }
 }
--- Puzzle_L3/src/puzzle_l3/CaseVide.java
+++ Puzzle_L3/src/puzzle_l3/CaseVide.java
@@ -5,10 +5,53 @@
  */
 package puzzle_l3;
 
+import javafx.scene.canvas.GraphicsContext;
+
 /**
  *
  * @author kerya
  */
 public class CaseVide extends Tile{ 
     
+    public boolean move(DIRECTION dir)
+    {
+        boolean valid=false;
+        switch(dir)
+        {
+            case DIRECTION.UP:
+                if(getPos().getY()!=0)
+                {
+                    getBoard().swapTiles(pos,new PosInt(pos.getX(),pos.getY()-1));
+                    valid=true;
+                }
+                break;
+            case DIRECTION.RIGHT:
+                if(getPos().getX()!=getBoard().getWidth()-1)
+                {
+                    getBoard().swapTiles(pos,new PosInt(pos.getX()+1,pos.getY()));
+                    valid=true;
+                }
+                break;
+            case DIRECTION.DOWN:
+                if(getPos().getY()!=getBoard().getHeight()-1)
+                {
+                    getBoard().swapTiles(pos,new PosInt(pos.getX(),pos.getY()+1));
+                    valid=true;
+                }
+                break;
+            case DIRECTION.LEFT:
+                if(getPos().getX()!=0)
+                {
+                    getBoard().swapTiles(pos,new PosInt(pos.getX()-1,pos.getY()));
+                    valid=true;
+                }
+                break;
+        }
+        return valid;
+    }
+    
+    public void draw(GraphicsContext context)
+    {
+        
+    }
 }
