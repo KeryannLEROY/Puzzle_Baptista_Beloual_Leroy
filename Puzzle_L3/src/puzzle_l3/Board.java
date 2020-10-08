0fb1731cf4e1ac7f8b7b002c6bf0c9d47f9bdaca
Mehdi <Mehdi@169.254.224.227>
2020-10-08 10:29:03 +0200

    Avancement sur la classe "Board"

--- Puzzle_L3/src/puzzle_l3/Board.java
+++ Puzzle_L3/src/puzzle_l3/Board.java
@@ -10,5 +10,51 @@ package puzzle_l3;
  * @author Mehdi
  */
 public class Board {
+    int width;
+    int height;
+    Tile tabTiles [][];
+    Tile vecTiles [];
+    
+    public int getWidth(){
+        return width;
+    }
+    
+    public int getHeight(){
+        return height;
+    }
+    
+    public boolean isCompleted(){
+        boolean completed=true;
+        for (int i; i<(width*height);i++){
+            //while (boolean completed){
+           //tile tab.checkpla
+        }
+       
+        
+    }
+    
+    
+    
+    
+    /*Méthode our permettre l'échange avec une case qui a la possiblité de bouger
+    et la case vide.
+    */ 
+    public void swapTiles(Tile t1,Tile t2){
+        Tile temp = t1;
+        t1 = t2;
+        t2= temp; 
+    }
+    
+    public void shuffle(Board b){
+        
+        
+    }
+    
+    public void draw(){
+        
+    }
+    
+    /*constructeur */
+    
     
 }
